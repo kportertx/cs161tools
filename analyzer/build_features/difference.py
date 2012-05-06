db8b148b913b6d18b8615003dbd9f5eeade48f0c
Kevin Porter <kporter@berkeley.edu>
2012-05-05 20:01:39 -0700

    difference can now take a directory as second argument yeilding all of feature1 that is not in common with any file in the path of feature2

--- analyzer/build_features/difference.py
+++ analyzer/build_features/difference.py
@@ -10,21 +10,41 @@ if len(sys.argv) < 2:
 feat1 = sys.argv[1]
 feat2 = sys.argv[2]
 
-if not (os.path.isfile(feat1) and os.path.isfile(feat2)):
+if not (os.path.isfile(feat1) and (os.path.isfile(feat2) or os.path.isdir(feat2))):
     print "feature1 and feature2 must both be a file"
 
-f1 = open(feat1, 'r')
-f2 = open(feat2, 'r')
+result = []
+if os.path.isfile(feat2):
+    f1 = open(feat1, 'r')
+    f2 = open(feat2, 'r')
+    
+    fset1 = set(f1.readlines())
+    fset2 = set(f2.readlines())
 
-fset1 = set(f1.readlines())
-fset2 = set(f2.readlines())
+    f1.close()
+    f2.close()
+    
+    rset = fset1 - fset2
+    result = sorted(list(rset))
+else:
+    f1 = open(feat1, 'r')
+    fset1 = set(f1.readlines())
+    f1.close()
+    
+    ls = os.listdir(feat2)
+    for file_name in ls:
+        file_name = feat2 + "/" + file_name
+        if os.path.isfile(file_name):
+            if os.path.getsize(file_name) < 300:
+                continue
+            f2 = open(file_name, 'r')
+            fset2 = set(f2.readlines())
+            f2.close()
+            rset = fset1 - fset2
+            fset1 = rset
+    result = sorted(list(fset1))
+    
 
-f1.close()
-f2.close()
-
-rset = fset1 - fset2
-
-result = sorted(list(rset))
 
 for r in result:
     print r.strip()
