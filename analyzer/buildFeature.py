121858de53986bcd6bda3659369da62e370998d8
Kevin Porter <kporter@berkeley.edu>
2012-05-08 00:02:47 -0700

    Jaccard index idea... was not the right direction

--- analyzer/buildFeature.py
+++ analyzer/buildFeature.py
@@ -60,7 +60,6 @@ def processFeature(feat):
             continue
 
         trace_set = set()
-        intersect = None
         for trace in traces: # Union all the traces
             f = open(trace, 'r')
             for l in f:
@@ -68,12 +67,7 @@ def processFeature(feat):
                 if (i and s[0] == 'i') or (dw and s[0:2] == 'dw') or (dr and s[0:2] == 'dr'):
                     trace_set.add(l)
             f.close()
-            if intersect is None:
-                intersect = trace_set.copy()
-            else:
-                intersect &= trace_set
         
-        trace_set -= intersect
         if target_set is None:
             target_set = trace_set
         else:
